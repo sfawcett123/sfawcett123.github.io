2d88a2e378c61738feb08b5c92645798bd3a293f
Steven Fawcett <64777084+sfawcett123@users.noreply.github.com>
2025-06-03 22:05:12 +0100

    YAML plugin working

--- _plugins/network.rb
+++ _plugins/network.rb
@@ -1,13 +1,23 @@
+require 'net/http'
+require 'uri'
+require 'yaml'
+
 module Reading
   class Generator < Jekyll::Generator
     safe true
+    def open(url)
+      Net::HTTP.get(URI.parse(url))
+    end
+
     def generate(site)
       config_value = Jekyll.configuration({})['yaml']
       print( "Configuring #{config_value[0]['filename']}\n" )
       config_value.each do | yaml|
-        site.data["network"] = {"name": "cheese" }
+        yaml_content = open( yaml['filename'])
+        yaml_data = YAML::load(yaml_content)
+        site.data[ yaml['name'] ] = yaml_data
       end
     end
   end
-
 end
+
